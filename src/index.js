9e6a23d2e4420c95e0fda0fbec9de5f5a5378773
layla  hammoud <laylahammoud63@gmail.com>
2021-03-16 11:19:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 iimport React from 'react';
 import ReactDOM from 'react-dom';
- 
+
 import ClickityClick from './components/ClickityClick';
- 
+
 ReactDOM.render(<ClickityClick />, document.getElementById('root'));
